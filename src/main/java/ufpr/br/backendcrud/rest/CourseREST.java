777f045fa6123f6d3e2b13734e7ee258ddd05427
danielmachado <danielmachadopintos@gmail.com>
2024-09-17 14:20:05 -0300

    feat: application crud

--- src/main/java/ufpr/br/backendcrud/rest/CourseREST.java
+++ src/main/java/ufpr/br/backendcrud/rest/CourseREST.java
@@ -1,10 +1,24 @@
 package ufpr.br.backendcrud.rest;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ufpr.br.backendcrud.model.Course;
+import ufpr.br.backendcrud.repository.CourseRepository;
 
 @CrossOrigin
 @RestController
 public class CourseREST {
+    @Autowired
+    private CourseRepository courseRepository;
 
+    @GetMapping("courses")
+    public ResponseEntity<List<Course>> listar() {
+        return ResponseEntity.ok(courseRepository.findAll());
+    }
 }
--- src/main/java/ufpr/br/backendcrud/rest/StudentREST.java
+++ src/main/java/ufpr/br/backendcrud/rest/StudentREST.java
@@ -1,10 +1,23 @@
 package ufpr.br.backendcrud.rest;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ufpr.br.backendcrud.model.Student;
+import ufpr.br.backendcrud.repository.StudentRepository;
 
 @CrossOrigin
 @RestController
 public class StudentREST {
+    @Autowired
+    private StudentRepository studentRepository;
 
+    @GetMapping("students")
+    public ResponseEntity<List<Student>> listar() {
+        return ResponseEntity.ok(studentRepository.findAll());
+    }
 }
